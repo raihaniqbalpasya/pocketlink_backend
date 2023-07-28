a711ca565768f3291d4124990858be8a4c1550c6
unknown <19104030@ittelkom-pwt.ac.id>
2023-07-28 16:19:29 +0700

    create database connection

--- config/database.js
+++ config/database.js
@@ -16,10 +16,10 @@ module.exports = {
     dialect: "postgres",
   },
   production: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
+    username: "ikzqewwn",
+    password: "F5u_H_A5pstE8e8uqkp3GhJwv0s2w0Ga",
+    database: "ikzqewwn",
+    host: "rosie.db.elephantsql.com",
     dialect: "postgres",
   },
 };
