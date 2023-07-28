b1a589a0dfd6c4a8607abc54e87c7ff95f3cd460
unknown <19104030@ittelkom-pwt.ac.id>
2023-07-28 17:24:16 +0700

    fix authorization

--- middlewares/userMiddleware.js
+++ middlewares/userMiddleware.js
@@ -10,18 +10,14 @@ module.exports = {
         return res.status(401).json({ message: "Token not found." });
       }
 
-      jwt.verify(
-        token,
-        "o4k5n43n5o3n2p3n5pm3mp99fgnl4dmblwq4m3",
-        (err, user) => {
-          if (err) {
-            return res.status(403).json({ message: "Token is not valid." });
-          }
-
-          req.user = user; // Menyimpan data pengguna dari token di req.user
-          next();
+      jwt.verify(token, process.env.ACCESS_TOKEN, (err, user) => {
+        if (err) {
+          return res.status(403).json({ message: "Token is not valid." });
         }
-      );
+
+        req.user = user; // Menyimpan data pengguna dari token di req.user
+        next();
+      });
     } catch (error) {
       if (error.message.includes("jwt expired")) {
         res.status(401).json({ message: "Token Expired" });
